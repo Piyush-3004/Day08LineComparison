39cd9873d36ef1ff1e78561db013f2d33de868ba
Chetan Patil <piyushp0541@gmail.com>
2022-07-03 23:43:54 +0530

    welcome to line comparison

--- src/LineComparison.java
+++ src/LineComparison.java
@@ -2,16 +2,6 @@
 public class LineComparison {
 
 	public static void main(String[] args) {
-		long x1, x2, y1, y2;
-		x1 = 0;
-		y1 = 1;
-		x2 = 2;
-		y2 = 3;
-		double len1;
-
-		len1 = Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
-
-		System.out.println("Length of line is : " + len1);
-
+		System.out.println("Welcome To Line Comparison Computation");
 	}
 }
