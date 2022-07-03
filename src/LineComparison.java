2af143db48de6cf568bf6261d95f844913bfe718
Chetan Patil <piyushp0541@gmail.com>
2022-07-03 23:37:27 +0530

    Equality of lines

--- src/LineComparison.java
+++ src/LineComparison.java
@@ -1,17 +1,50 @@
+import java.util.Scanner;
 
 public class LineComparison {
 
 	public static void main(String[] args) {
-		long x1, x2, y1, y2;
-		x1 = 0;
-		y1 = 1;
-		x2 = 2;
-		y2 = 3;
-		double len1;
+		long nx1, nx2, ny1, ny2;
+		double len;
+		nx1 = 0;
+		ny1 = 1;
+		nx2 = 2;
+		ny2 = 3;
 
-		len1 = Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+		len = Math.sqrt((nx2 - nx1) * (nx2 - nx1) + (ny2 - ny1) * (ny2 - ny1));
 
-		System.out.println("Length of line is : " + len1);
+		System.out.println("Length of line is : " + len);
+
+		System.out.println("Enter coordinates to check if the line is equalto line one");
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("enter x1");
+
+		int x1 = sc.nextInt();
+
+		System.out.println("enter y1");
+
+		int y1 = sc.nextInt();
+
+		System.out.println("enter x2");
+
+		int x2 = sc.nextInt();
+
+		System.out.println("enter y2");
+
+		int y2 = sc.nextInt();
+
+		double len1 = Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+
+		System.out.println("distanc : " + len1);
+		Double lengthOne = new Double(len);
+		Double lengthTwo = new Double(len1);
+
+		if (lengthOne.equals(lengthTwo)) {
+			System.out.println("Line 2 is equal to line one");
+		}
+
+		sc.close();
 
 	}
 }
